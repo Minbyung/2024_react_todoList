4b70d2c84f5ccd4627d7d2d6bf59600d390e613b
Minbyung <mbmcybery@gmail.com>
2024-04-29 19:53:39 +0900

    페이지 분리 및 초기 UI 작업 3

--- src/compoents/pages/MainPage.js
+++ src/compoents/pages/MainPage.js
@@ -1,11 +1,19 @@
+import TodosEmpty from "../TodosEmpty";
+
 export default function MainPage() {
-    return (
-      <>
-        <div className="flex-1 flex justify-center items-center">
-          <div>
-            <span>메인 페이지</span>
-          </div>
-        </div>
-      </>
-    );
+  const todosEmpty = true;
+
+  if ( todosEmpty ) {
+    return <TodosEmpty />;
   }
+
+  return (
+    <>
+      <div className="flex-1 flex justify-center items-center">
+        <div>
+          <span>메인 페이지</span>
+        </div>
+      </div>
+    </>
+  );
+}
