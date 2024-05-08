40b6407d67359b3a477d8b24f89bf953c2a63320
Minbyung <mbmcybery@gmail.com>
2024-05-08 20:14:29 +0900

    나머지 정렬 기능 구현

--- src/compoents/pages/TodoListPage.js
+++ src/compoents/pages/TodoListPage.js
@@ -29,11 +29,35 @@ export default function TodoList() {
 
     const filteredTodos = getFilteredTodos();
 
-    const sortedTodos = [...filteredTodos].sort((a, b) => {
-      if ( a.performDate == b.performDate ) return 0;
+    const getSortedTodos = () => {
+      if ( sortIndex == 0 ) {
+        return [...filteredTodos].sort((a, b) => {
+          if ( a.performDate == b.performDate ) return 0;
+    
+          return a.performDate > b.performDate ? 1 : -1; 
+        });
+      } 
 
-      return a.performDate > b.performDate ? 1 : -1; 
-    })
+      else if ( sortIndex == 1 ) {
+        return [...filteredTodos].sort((a, b) => {
+          if ( a.performDate == b.performDate ) return 0;
+    
+          return a.performDate < b.performDate ? 1 : -1; 
+        });
+      }
+      
+      else if ( sortIndex == 2 ) {
+        return [...filteredTodos].sort((a, b) => {
+          if ( a.id == b.id ) return 0;
+    
+          return a.id > b.id ? 1 : -1; 
+        });
+      }
+
+      return filteredTodos;
+    }
+
+    const sortedTodos = getSortedTodos();
   
     return (
       <>
@@ -124,7 +148,7 @@ export default function TodoList() {
           }
           value={3}
         />
-      </Tabs>
+        </Tabs>
   
         <div className='mt-4 px-4'>
           <ul>
